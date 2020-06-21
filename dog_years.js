32bf2150c6cd661339a0f91581d672dd93bcd87a
josantcogsci <ja.mendozachavez@gmail.com>
2020-06-21 13:58:46 -0500

    dog_years

---
+++ dog_years.js
@@ -0,0 +1,13 @@
+// This is my age
+let myAge = 33;
+
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+laterYears = (myAge - 2);
+laterYears *= 4;
+
+myAgeInDogYears = earlyYears + laterYears;
+
+myName = "José".toLowerCase();
+console.log("My name is " + myName + "." + " I am " + myAge + " years old in human years which is " + myAgeInDogYears + ". years old.");
